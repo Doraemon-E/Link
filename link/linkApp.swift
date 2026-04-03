772af76701c1a83007c30644134d98f324c5f260
Doraemon-E <tchen.life@gmail.com>
2026-04-03 23:39:34 +0800

    Initial Commit

---
+++ link/linkApp.swift
@@ -0,0 +1,17 @@
+//
+//  linkApp.swift
+//  link
+//
+//  Created by Doracmon on 2026/4/3.
+//
+
+import SwiftUI
+
+@main
+struct linkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
